ac2f3f60b17cda5f8b91188a9d85b75679627152
ti <ingenieriart3@gmail.com>
2025-10-29 16:35:20 -0300

    deploy

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './js/**/*.js',
+    '../lib/laura_web/**/*.{heex,ex,exs}',
     '../lib/laura_web/components/**/*.{heex,ex}',
     '../lib/laura_web/controllers/**/*.{heex,ex}',
     '../lib/laura_web/live/**/*.{heex,ex}',
