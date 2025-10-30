5e03ead7a27c17aaf95950c31da07991ee78c506
ti <ingenieriart3@gmail.com>
2025-10-30 18:21:01 -0300

    estabilizo register

--- lib/laura/platform.ex
+++ lib/laura/platform.ex
@@ -13,6 +13,10 @@ defmodule Laura.Platform do
     |> Repo.insert()
   end
 
+  def change_health_brand(%HealthBrand{} = health_brand, attrs \\ %{}) do
+    HealthBrand.registration_changeset(health_brand, attrs)
+  end
+
   def update_health_brand(%HealthBrand{} = health_brand, attrs) do
     health_brand
     |> HealthBrand.changeset(attrs)
--- lib/laura_web/controllers/landing_controller.ex
+++ lib/laura_web/controllers/landing_controller.ex
@@ -1,19 +1,45 @@
-# lib/laura_web/controllers/landing_controller.ex
+# # lib/laura_web/controllers/landing_controller.ex
+# defmodule LauraWeb.LandingController do
+#   use LauraWeb, :controller
+
+#   def index(conn, _params) do
+#     plans = Laura.Billing.list_public_subscription_plans()
+#     render(conn, :index, plans: plans)
+#   end
+
+#   def pricing(conn, _params) do
+#     plans = Laura.Billing.list_public_subscription_plans()
+#     render(conn, :pricing, plans: plans)
+#   end
+
+#   def features(conn, _params) do
+#     render(conn, :features)
+#   end
+
+#   def contact(conn, _params) do
+#     render(conn, :contact)
+#   end
+# end
+
 defmodule LauraWeb.LandingController do
   use LauraWeb, :controller
+  alias Laura.Billing
 
+  # def index(conn, _params) do
+  #   render(conn, :index)
+  # end
   def index(conn, _params) do
     plans = Laura.Billing.list_public_subscription_plans()
     render(conn, :index, plans: plans)
   end
 
-  def pricing(conn, _params) do
-    plans = Laura.Billing.list_public_subscription_plans()
-    render(conn, :pricing, plans: plans)
-  end
-
   def features(conn, _params) do
     render(conn, :features)
+  end
+
+  def pricing(conn, _params) do
+    plans = Billing.list_public_subscription_plans()
+    render(conn, :pricing, plans: plans, current_staff: conn.assigns[:current_staff])
   end
 
   def contact(conn, _params) do
